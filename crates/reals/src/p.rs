712657f088639690b40f864fb6f0426c8df97115
Rune Tynan <runetynan@gmail.com>
2023-08-23 10:07:43 -0400

    Start adding posits

--- crates/reals/src/p.rs
+++ crates/reals/src/p.rs
@@ -2,6 +2,7 @@
 
 use core::cmp::Ordering;
 use core::ops::{Add, Neg, Sub};
+use numeric_bits::bit_slice::BitSliceExt;
 
 /// N-byte posit value.
 ///
@@ -16,12 +17,17 @@ use core::ops::{Add, Neg, Sub};
 /// This implementation is conformant with the 2022 Posit Standard, where the `n` of a posit
 /// is `N * 8` of this type. As such, `P<1> == posit8`, `P<4> == posit32`, etc.
 // [S|R..|R0|EE|F..]
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub struct P<const N: usize>([u8; N]);
 
 impl<const N: usize> P<N> {
-    #[allow(unused)] // TODO: Use this
-    const FRAC_LEN: usize = N * 8 - 5;
+    const FRAC_LEN: usize = N*8 - 5;
+
+    const NAR: Self = {
+        let mut out = P([0; N]);
+        out.0[0] | 0x1;
+        out
+    };
 
     /// Create a new posit with value `0`.
     pub const fn new() -> P<N> {
@@ -38,11 +44,35 @@ impl<const N: usize> P<N> {
     pub fn is_nar(self) -> bool {
         self.0[0] == 0x1 && self.0[1..].iter().all(|&b| b == 0)
     }
+
+    pub fn next_up(self) -> P<N> {
+        // Implement as two's complement +1
+        todo!()
+    }
+
+    pub fn next_down(self) -> P<N> {
+        // Implement as two's complement -1
+        todo!()
+    }
+
+    fn regime(self) -> (bool, usize) {
+        let mut iter = self.0.iter_bits().skip(1);
+        let first = iter.next().unwrap();
+        let count = iter.take_while(|&v| v == first)
+            .count();
+        (first, count)
+    }
 }
 
 impl<const N: usize> Default for P<N> {
     fn default() -> Self {
         P::new()
+    }
+}
+
+impl<const N: usize> From<i32> for P<N> {
+    fn from(value: i32) -> Self {
+        todo!()
     }
 }
 
@@ -54,6 +84,7 @@ impl<const N: usize> PartialOrd for P<N> {
 
 impl<const N: usize> Ord for P<N> {
     fn cmp(&self, _: &Self) -> Ordering {
+        // Implement as two's complement <=>
         todo!()
     }
 }
@@ -62,6 +93,7 @@ impl<const N: usize> Neg for P<N> {
     type Output = P<N>;
 
     fn neg(mut self) -> Self::Output {
+        // TODO: This may be wrong, 2's complement
         self.0[0] ^= 1;
         self
     }
@@ -69,9 +101,53 @@ impl<const N: usize> Neg for P<N> {
 
 impl<const N: usize> Add for P<N> {
     type Output = P<N>;
+    
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self.is_zero(), rhs.is_zero()) {
+            (true, _) => return rhs,
+            (_, true) => return self,
+            _ => (),
+        };
 
-    fn add(self, _: Self) -> Self::Output {
-        todo!()
+        if self.is_nar() || rhs.is_nar() {
+            return Self::NAR
+        }
+
+        // S = s
+        // r = if R0 == 0 { -k } else { k-1 }
+        // e = E
+        // f = F / 2^m
+        // Value: P = ((1-3s)+f) * 2^((1-2s)*(4r+e+s))
+
+        // 1-3s == 1 or -2
+        // 1-2s == 1 or -1
+
+        // When positive, we count fraction up from 1 to 2
+        // When R0 is 1, it and exponent form power steps by 2 up from 1 (1, 2, 4)
+        // When R0 is 0, it and exponent form power steps by 2 down from 1 (1, 1/2, 1/4)
+        //    with additional note that exponent is always added - so on this side, we start at regime -1,
+        //    exponent 3, then move exponent down till we tick over regime
+
+        // When negative, we count fraction down from -2 to -1 (two's complement)
+        // When R0 is 0, it and exponent form power steps by 2 up from 1 (1, 2, 4)
+        //    with additional note that exponent is always added - so on this side, we start at regime -1,
+        //    exponent 3, then move exponent down till we tick over regime
+        // When R0 is 1, it and exponent form power steps by 2 down from 1 (1, 1/2, 1/4)
+
+        // Moving up from 1:
+        // When fraction rolls over, increment exponent and reset fraction
+        // When exponent rolls over, increment regime and reset exponent
+
+        // Moving down from 1:
+        // Inverted regime
+        // When fraction rolls over, decrement exponent and reset fraction
+
+        // next_up is just two's complement add 1
+        // next_down is just two's complement sub 1
+
+        // Addition is more complex - start with just all positives:
+        //   Sum will be no more precise than the larger exponent
+        //   Take 'theoretical' values, sum them, round
     }
 }
 
@@ -98,3 +174,33 @@ pub type p32 = P<4>;
 /// 64-bit posit. Range of `[2^-248, 2^248]`, with max integer value of `2^49` (562,949,953,421,312)
 #[allow(non_camel_case_types)]
 pub type p64 = P<8>;
+
+/// N-byte posit quire value.
+///
+/// A quire is an intermediate data structure used for performing multiple mathematical operations
+/// on posits in succession without incurring precision drift. In other words, a quire allows
+/// 'batching' operations before pulling out a result with only one rounding applied, to the final
+/// result.
+///
+/// ## Format
+///
+/// This implementation is conformant with the 2022 Posit Standard, where `Quire<N>` is the quire
+/// for [`P<N>`]. As such, `Quire<1>` is 128 bits, `Quire<4>` is 512 bits, etc.
+// [S|C..|I..|F..]
+pub struct Quire<const N: usize>([u128; N]);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let p1 = p32::from(1);
+        let p2 = p32::from(-1);
+
+        assert_eq!(p1 + p1, p32::from(2));
+        assert_eq!(p1 + p2, p32::from(0));
+        assert_eq!(p2 + p1, p32::from(0));
+        assert_eq!(p2 + p2, p32::from(-2));
+    }
+}
