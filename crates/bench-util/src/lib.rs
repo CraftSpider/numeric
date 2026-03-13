1ce0ff6b1834f3f1efb73381112e2a2df58bbcce
Rune Tynan <runetynan@gmail.com>
2026-03-13 10:01:18 -0700

    Fix up tracing allocator, use workspace criterion

--- crates/bench-util/src/lib.rs
+++ crates/bench-util/src/lib.rs
@@ -2,8 +2,7 @@
 
 use criterion::Criterion;
 #[cfg(unix)]
-use pprof::criterion::{Output, PProfProfiler};
-
+// use pprof::criterion::{Output, PProfProfiler};
 #[cfg(feature = "alloc-measures")]
 pub mod alloc_measure;
 pub mod trace_alloc;
@@ -12,7 +11,7 @@ pub mod trace_alloc;
 /// compiling on unix targets.
 pub fn make_criterion() -> Criterion {
     let c = Criterion::default();
-    #[cfg(unix)]
-    let c = c.with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    // #[cfg(unix)]
+    // let c = c.with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
     c.configure_from_args()
 }
